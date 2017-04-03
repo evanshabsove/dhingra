caca5735ab5a949e3d26ed1e0aebb008f78432c3
Evan Shabsove <evan.shabsove@gmail.com>
2017-04-03 17:30:49 -0400

    added all needed routes, home nested resource of paintings and gallaries, about contact and blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :gallaries do
+    resources :paintings
+  end
+
+  root 'home#index'
+
+  get '/about', to: 'home#about', as: 'about'
+
+  get '/contact' to: 'home#contact', as: 'contact'
+
+  get '/blog' to: 'home#blog', as: 'blog'
 end
