411a227c1f7f1c41c1e19bdb0b7ecef22c7c0cf7
Evan Shabsove <evan.shabsove@gmail.com>
2017-04-03 17:26:16 -0400

    generated all controllers, no making routes

---
+++ app/controllers/gallaries_controller.rb
@@ -0,0 +1,2 @@
+class GallariesController < ApplicationController
+end
---
+++ test/controllers/gallaries_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GallariesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
