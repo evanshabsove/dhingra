73214795f4432659780fe78e0085eeb8ed46f0e5
DAVID <davidnmwood@gmail.com>
2017-04-03 16:47:48 -0700

    Setup controller CRUD for paintings

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,2 +1,47 @@
 class PaintingsController < ApplicationController
+
+  def show
+    @painting = Painting.find(params[:id])
+  end
+
+  def new
+    @painting = Painting.new
+  end
+
+  def edit
+    @painting = Painting.find(params[:id])
+  end
+
+  def create
+    @painting = Painting.new(painting_params)
+
+    if @painting.save
+      redirect_to gallaries_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @painting = Painting.find(params[:id])
+    if @painting.update_attributes(painting_params)
+      redirect_to gallaries_url
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @gallery = Gallary.find(params[:id])
+    if @gallery.destroy
+      redirect_to gallaries_url
+    end
+  end
+
+  #Private params
+  private
+  def painting_params
+    params.require(:painting).permit(:title, :description, :image)
+  end
+
 end
