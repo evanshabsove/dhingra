ebb509eeb442a60b399bc3cb85f78b99cdabffc0
Evan Shabsove <evan.shabsove@gmail.com>
2017-04-11 21:57:13 -0400

    removed commented code

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,23 +1,5 @@
 $( document ).on('turbolinks:load', function() {
 
-  // $("#create_painting_button").on("submit", function(event){
-  //   event.preventDefault();
-  //   var parent = $(this).parent()
-  //   var id = parent.attr("id")
-  //   console.log("here");
-  //   console.log($(this).serialize());
-  //   $.ajax({
-  //     url: "/galleries/" + id + "/paintings/poop",
-  //     method: "post",
-  //     dataType: "JSON",
-  //     data: $(this).serialize()
-  //   }).done(function(responseData){
-  //     console.log(responseData.html);
-  //     console.log("hello");
-  //     $("#all_paintings").html(responseData.html)
-  //   });
-  // });
-
   $(document).on('click','#create_painting_button', {}, function(){
     event.preventDefault();
     var parent = $(this).parent()
@@ -28,7 +10,6 @@ $( document ).on('turbolinks:load', function() {
       dataType: "JSON",
       data: $("#new_painting").serialize()
     }).done(function(responseData){
-      console.log(responseData.html);
       $("#all-paintings").html(responseData.html)
     });
   });
