068e7a167dd09f7f3f9ce27cdeb53a6e35d54565
Evan Shabsove <evan.shabsove@gmail.com>
2017-04-11 21:45:37 -0400

    ajax request actually working now, will fix response data

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,19 +1,33 @@
 $( document ).on('turbolinks:load', function() {
 
-  $("#create_painting_button").on("submit", function(event){
+  // $("#create_painting_button").on("submit", function(event){
+  //   event.preventDefault();
+  //   var parent = $(this).parent()
+  //   var id = parent.attr("id")
+  //   console.log("here");
+  //   console.log($(this).serialize());
+  //   $.ajax({
+  //     url: "/galleries/" + id + "/paintings/poop",
+  //     method: "post",
+  //     dataType: "JSON",
+  //     data: $(this).serialize()
+  //   }).done(function(responseData){
+  //     console.log(responseData.html);
+  //     console.log("hello");
+  //     $("#all_paintings").html(responseData.html)
+  //   });
+  // });
+
+  $(document).on('click','#create_painting_button', {}, function(){
     event.preventDefault();
     var parent = $(this).parent()
     var id = parent.attr("id")
-    console.log("here");
-    console.log($(this).serialize());
     $.ajax({
       url: "/galleries/" + id + "/paintings",
       method: "post",
       dataType: "JSON",
-      data: $(this).serialize()
+      data: $("#new_painting").serialize()
     }).done(function(responseData){
-      console.log(responseData.html);
-      console.log("hello");
       $("#all_paintings").html(responseData.html)
     });
   });
