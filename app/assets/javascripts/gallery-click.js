98087f1d102eb2e46b7bd3a6b9be97c461e17b58
Tedmaloney <Tedmaloney@me.com>
2017-04-24 14:41:34 -0300

    Added hover over on each div

---
+++ app/assets/javascripts/gallery-click.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".gallery-painting").on('mouseover', function() {
+    $(this).siblings(1).fadeIn("fast");
+  })
+  $(".gallery-painting").on("mouseout", function() {
+    $(this).siblings(1).fadeOut("fast");
+
+  })
+})
