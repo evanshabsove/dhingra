3a74b850b07308f7613ab9b6d6e5432716b3b406
Evan Shabsove <evan.shabsove@gmail.com>
2017-05-29 19:18:06 -0400

    added text below image that changes bassed on what image you are on

--- app/assets/javascripts/gallery-slick.js
+++ app/assets/javascripts/gallery-slick.js
@@ -6,4 +6,17 @@ $( document ).on('turbolinks:load', function() {
     slidesToShow: 3,
     slidesToScroll: 3
   });
+
+  value = $(".slick-current").attr("class")[0]
+  $("#" + value).fadeIn()
+
+  $(".center").on('afterChange', function(event, slick, direction){
+    value = $(".slick-current").attr("class")[0]
+    $("#" + value).fadeIn()
+  })
+
+  $(".center").on('beforeChange', function(event, slick, direction){
+    value = $(".slick-current").attr("class")[0]
+    $("#" + value).fadeOut()
+  })
 });
