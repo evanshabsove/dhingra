635d4249d891c7acd1d27a8810335359a0f19e64
Evan Shabsove <evan.shabsove@gmail.com>
2018-07-05 09:02:56 -0400

    can now edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,10 +17,24 @@ class ProductsController < ApplicationController
   end
 
   def update
-    #code
+    @product = Product.find(params[:id])
+    @product_group = @product.product_group
+    @province = @product_group.province
+    if @product.update_attributes(product_params)
+      flash[:success] = "Successfully updated product"
+      redirect_to edit_province_product_group_url(@province, @product_group)
+    else
+      flash[:error] = "Something went wrong Please try again"
+      redirect_to edit_province_product_group_url(@province, @product_group)
+    end
   end
 
   def destroy
     #code
+  end
+
+  private
+  def product_params
+    params.permit(:name, :price, :code, :min)
   end
 end
