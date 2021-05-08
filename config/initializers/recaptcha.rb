1cded73ac23af5c63eabe02f46df4d79c89912d7
Evan <evan@spectrum.md>
2021-05-08 11:55:40 -0400

    Trying this out

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
-Recaptcha.configure do |config|
-  config.site_key  = ENV['re_captcha_public_key']
-  config.secret_key = ENV['re_captcha_private_key']
-end
+# Recaptcha.configure do |config|
+#   config.site_key  = ENV['re_captcha_public_key']
+#   config.secret_key = ENV['re_captcha_private_key']
+# end
