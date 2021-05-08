e79e3871b1b6e3414e3bcb56efbc9a13954b59a5
Evan <evan@spectrum.md>
2021-05-08 12:08:36 -0400

    Follow the guide you dummy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,13 @@ class ApplicationController < ActionController::Base
     dashboard_index_path
   end
 
+  def verify_recaptcha?(token, recaptcha_action)
+    secret_key = Rails.application.credentials.dig(:recaptcha_secret_key)
+
+    uri = URI.parse("https://www.google.com/recaptcha/api/siteverify?secret=#{secret_key}&response=#{token}")
+    response = Net::HTTP.get_response(uri)
+    json = JSON.parse(response.body)
+    json['success'] && json['score'] > RECAPTCHA_MINIMUM_SCORE && json['action'] == recaptcha_action
+  end
+
 end
